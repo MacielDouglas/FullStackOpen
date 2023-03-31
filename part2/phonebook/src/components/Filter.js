791124053fe344914da8ea3775b6c9a2ae3dd670
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-03-31 10:38:38 -0300

    terminado exercicios 2.10, refatorado em componentes Filter, Persons e PersonsForm

--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Filter = () => {
-  return <div>Filter</div>;
+const Filter = ({ filterInput, handleFilterChange }) => {
+  return (
+    <div>
+      <form>
+        <div>
+          filter shown with:
+          <input value={filterInput} onChange={handleFilterChange} />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Filter;
