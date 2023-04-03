e65c4a5fa982098f78b091161e12d46b4e5144c0
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-04-03 10:14:57 -0300

    Implementando modulo service, criado o get

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
+
+import personsService from './services/persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  // ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterInput, setFilterInput] = useState('');
+
+  useEffect(() => {
+    personsService.getAll().then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   console.log('effect: (efeito)');
+  //   axios.get('http://localhost:3001/persons').then((response) => {
+  //     console.log('promise fulfilled (promessa resolvida)');
+  //     setPersons(response.data);
+  //   });
+  // }, []);
+  // console.log('render (renderiza)', persons.length, 'persons (pessoas)');
 
   const addNewPerson = (event) => {
     event.preventDefault();
@@ -29,9 +48,14 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(newPerson));
-      setNewName('');
-      setNewNumber('');
+      axios
+        .post('http://localhost:3001/persons', newPerson)
+        .then((response) => {
+          console.log(response);
+          setPersons(persons.concat(newPerson));
+          setNewName('');
+          setNewNumber('');
+        });
     }
   };
 
